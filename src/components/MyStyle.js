0a746d9661f6c183d675f07808dac2311ab724bf
ayomisco <fayomide324@gmail.com>
2024-06-02 17:41:16 +0100

    First commit with changes

---
+++ src/components/MyStyle.js
@@ -0,0 +1,31 @@
+
+
+import styled from 'styled-components';
+
+
+export const Title1 = styled.h1`
+font-size : 60px;
+font-family: arial;
+
+`
+
+export const Container = styled.div`
+background-color: red;
+color: white;
+width: 200px;
+height: 200px;
+border: 2px;
+`
+
+export const NavUl = styled.ul`
+background : grey;
+width: 200px;
+height: 90px;
+color: brown;
+`
+
+export const Navli = styled.li`
+
+
+
+`
---
+++ src/components/home.js
@@ -0,0 +1,20 @@
+import { Title1 } from "./MyStyle"
+import { Container } from "./MyStyle"
+import { Button } from "./MyStyle"
+
+
+export const Header1 = () => {
+    return(
+        <>
+        <Container>
+
+        <Title1> THe World Story </Title1>
+        
+
+        </Container>
+
+        <Button> Click Me </Button >
+
+        </>
+    )
+}
